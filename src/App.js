0a75eb961ade17de8d5e780e639c1a45be88c2db
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2020-04-20 17:15:53 +0000

    initial setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import styles from "./App.module.scss";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
